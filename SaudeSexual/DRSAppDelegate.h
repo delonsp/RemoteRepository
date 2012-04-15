201106afa567daa4914bfe80f8c585903ff51315
Alain Machado da Silva Dutra <alaindutra@imac-de-alain.(none)>
2012-04-15 20:10:17 -0300

    Initial Commit

---
+++ SaudeSexual/DRSAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DRSAppDelegate.h
+//  SaudeSexual
+//
+//  Created by Alain Machado da Silva Dutra on 15/04/12.
+//  Copyright (c) 2012 DrSolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DRSViewController;
+
+@interface DRSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) DRSViewController *viewController;
+
+@end
---
+++ SaudeSexual/DRSViewController.h
@@ -0,0 +1,13 @@
+//
+//  DRSViewController.h
+//  SaudeSexual
+//
+//  Created by Alain Machado da Silva Dutra on 15/04/12.
+//  Copyright (c) 2012 DrSolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DRSViewController : UIViewController
+
+@end
